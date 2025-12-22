859ce99559e524e7e2166b28a2c3277dc7e4d1a3
maheshartharu <156569657+maheshartharu@users.noreply.github.com>
2025-12-22 15:56:34 +0530

    Initial React project

---
+++ src/22-12-2025/first_class.jsx
@@ -0,0 +1,12 @@
+import  {Component} from "react";
+class GreatingClass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello From Class Component</h1>
+        <p>This is my first class component in React.</p>
+      </div>
+    );
+  }
+}
+export default GreatingClass;
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import GreatingClass from "./22-12-2025/first_class"
+
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello, React!</h1>
+      <br />
+
+      <input type="File" ></input>
+      <input type="Range" ></input>
+      <input type="Color" ></input>
+      <input type="Date" ></input>
+      <input type="Time" ></input>
+      <input type="calculate" ></input>
+      <br />
+      <GreatingClass />
+      
+    </>
+  )
+}
+export default App
